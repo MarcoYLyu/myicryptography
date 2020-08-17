e57b722786ccedf6ebe6709657ed7cf66a523c90
MarcoYLyu <myilyu19990707@gmail.com>
2020-08-16 23:23:50 -0700

    edited test.py

--- test.py
+++ test.py
@@ -6,12 +6,6 @@ Created on Wed Apr  1 00:00:11 2020
 @author: marcolyu
 """
 
-import myicrypto as algo
-
-bar = [[6513996, 6393464], [66586820, 65354729]]
-foo = algo.Lattice(bar)
-orthogonal_basis = foo.orthogonalize()
-
-pkc = algo.crypto.ElgamalPKC(467, 2)
-pkc.set_secret_key(153)
-print(pkc.decrypt(pkc.encrypt(331)))
+import scytale as algo
+import numpy as np
+import random
